ccb46a8197725e6e4d5970df4f9a95aae52cdca3
IAmDanielPeter <124053362+IAmDanielPeter@users.noreply.github.com>
2025-09-17 02:48:45 -0700

    guidebook

--- config/shared.lua
+++ config/shared.lua
@@ -3,8 +3,9 @@ return {
     defaultSpawn = vec4(-540.58, -212.02, 37.65, 208.88),
     notifyPosition = 'top-right', -- 'top' | 'top-right' | 'top-left' | 'bottom' | 'bottom-right' | 'bottom-left'
     ---@type { name: string, amount: integer, metadata: fun(source: number): table }[]
-    starterItems = { -- Character starting items
-        { name = 'money',   amount = 5000 },
-        { name = 'giftbox', amount = 1 }
+    starterItems = {              -- Character starting items
+        { name = 'money',             amount = 5000 },
+        { name = 'giftbox',           amount = 1 },
+        { name = 'moonlit_guidebook', amount = 1 }
     }
 }
