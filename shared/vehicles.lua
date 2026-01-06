4b35be1aa408bd3f39153ebb31a1ea54f23b5eb1
IAmDanielPeter <124053362+IAmDanielPeter@users.noreply.github.com>
2026-01-05 20:49:36 -0800

    new vehicle

--- shared/vehicles.lua
+++ shared/vehicles.lua
@@ -1141,4 +1141,5 @@ return {
     emperorncpd        = { hash = "emperorncpd", category = "emergency", brand = "Emperor", name = "Police", type = "automobile", price = 79879, shop = "none", model = "emperorncpd", },
     cortesnpcd         = { hash = "cortesnpcd", category = "emergency", brand = "Cortes", name = "Police", type = "automobile", price = 79879, shop = "none", model = "cortesnpcd", },
     pace_cypheranim    = { hash = "pace_cypheranim", category = "sports", brand = "Pace", name = "Cypher Anim", type = "automobile", price = 79879, shop = "none", model = "pace_cypheranim", },
+    hornethf           = { hash = "hornethf", category = "sports", brand = "Bravado", name = "Hornet HF", type = "automobile", price = 79879, shop = "none", model = "hornethf", },
 }
