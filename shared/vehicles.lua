90a928af9b42cdcd9d7112d49b50e94cceb281a4
IAmDanielPeter <124053362+IAmDanielPeter@users.noreply.github.com>
2026-08-21 15:57:36 -0700

    push fix to motocycle

--- shared/vehicles.lua
+++ shared/vehicles.lua
@@ -1154,4 +1154,5 @@ return {
     pace_hellbringer     = { model = 'pace_hellbringer', name = 'Hellbringer', brand = 'Pace', price = 0, category = 'bike', type = 'bike', hash = 'pace_hellbringer' },
     nullpulseway         = { model = 'nullpulseway', name = 'Pulseway', brand = 'Light Bee', price = 0, category = 'bike', type = 'bike', hash = 'nullpulseway' },
     dloader2             = { model = 'dloader2', name = 'Dloader 2', brand = 'Bravado', price = 0, category = 'utility', type = 'automobile', hash = 'dloader2' },
+    jormungandr          = { hash = "jormungandr", category = "motorcycles", brand = "Jormungandr", name = "Jormungandr", type = "bike", price = 0, shop = "none", model = "jormungandr", },
 }
